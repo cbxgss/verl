81da552804648e9bdf63cfb3e40f9d68abb7d5d6
chengrong <chengrong@tju.edu.cn>
2025-07-02 14:44:35 +0800

    w

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ install_requires = [
     "pylatexenc",
     "ray[default]>=2.41.0",
     "torchdata",
-    "tensordict<=0.6.2",
+    "tensordict<=0.6.1",
     "transformers",
     "wandb",
     "packaging>=20.0",
