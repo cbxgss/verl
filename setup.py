51f0a60f0e216f38ded7555e86f17774e581e828
chengrong <chengrong@tju.edu.cn>
2025-07-02 17:28:41 +0800

    w

--- setup.py
+++ setup.py
@@ -53,9 +53,9 @@ MATH_REQUIRES = ["math-verify"]  # Add math-verify as an optional dependency
 VLLM_REQUIRES = ["tensordict<=0.6.2", "vllm<=0.8.5"]
 SGLANG_REQUIRES = [
     "tensordict<=0.6.2",
-    "sglang[all]==0.4.8",
+    "sglang[all]==0.4.6.post5",
     "torch-memory-saver>=0.0.5",
-    "torch==2.7.1",
+    "torch==2.6.0",
 ]
 TRL_REQUIRES = ["trl<=0.9.6"]
 
