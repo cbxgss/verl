e32dda5e42244a3312c4b75c96e749bb6f869f92
chengrong <chengrong@tju.edu.cn>
2025-07-02 14:58:56 +0800

    w

--- train_grpo.sh
+++ train_grpo.sh
@@ -68,7 +68,6 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.rollout.max_num_batched_tokens=32768 \
     actor_rollout_ref.rollout.n=${n} \
-    actor_rollout_ref.rollout.multi_turn.max_turns=4 \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.95 \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
